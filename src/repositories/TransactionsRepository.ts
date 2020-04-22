1c13fb75b212ac98a3ae547232e2e249563a6089
Raphael <raphael.novello@gmail.com>
2020-04-22 18:40:02 -0300

    Done!

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,47 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    if (transaction.type === 'income') {
+      this.balance.income += value;
+      this.balance.total += value;
+    }
+    if (transaction.type === 'outcome') {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    }
+    return transaction;
   }
 }
 
